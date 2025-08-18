a4ba6a78b2ffbf333cadb2c9455a1ed1f0a7125a
developerthierry <developerthierry@gmail.com>
2025-08-18 04:21:58 +0000

    AnimatedTittle Page Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,10 @@ const About = () => {
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </h2>
-        <AnimatedTitle/>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
         
 
         <div className="about-subtext">
@@ -38,6 +41,8 @@ const About = () => {
           <p>Zentry unites every player from countless games and platforms</p>
         </div>
       </div>
+
+      
       <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
             <img src="img/about.webp" alt="Background" className="absolute left-0 top-0 size-full object-cover" />
