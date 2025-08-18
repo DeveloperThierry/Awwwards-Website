1efceb7553ad008ba02d242f212a21d487d3fe2c
developerthierry <developerthierry@gmail.com>
2025-08-18 03:51:59 +0000

    Created About Page

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Disc<b>o</b>ver the World's <br />L<b>a</b>rgest Shared Adventure
+    </div>
+  );
+};
+
+export default AnimatedTitle;
