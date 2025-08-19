6273572fe198d40e84ca7f91a98409eb21009b35
developerthierry <developerthierry@gmail.com>
2025-08-19 14:01:06 +0000

    Ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Features from './components/Features'
 import Story from './components/Story'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
@@ -14,6 +15,7 @@ const App = () => {
      <Features/>
      <Story/>
      <Contact/>
+     <Footer/>
     </main>
   )
 }
