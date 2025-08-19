6cd8cec560c196abadb01c47aad8218887ccfee8
developerthierry <developerthierry@gmail.com>
2025-08-19 13:35:20 +0000

    Story Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
@@ -10,6 +12,8 @@ const App = () => {
      <Hero/>
      <About/>
      <Features/>
+     <Story/>
+     <Contact/>
     </main>
   )
 }
